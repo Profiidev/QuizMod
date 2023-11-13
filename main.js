8974b24900e187c9effbaee0be0bd95b79bbd8bb
Profiidev <benjamin@familiebuerger.com>
2023-11-13 17:13:27 +0100

    Fixed Answer Requests

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch("https://api.quizacademy.io/business-nest/public/live_events/pin/LOSGFT", {
+/*fetch("https://api.quizacademy.io/business-nest/public/live_events/pin/LOSGFT", {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
@@ -22,4 +22,57 @@ fetch("https://api.quizacademy.io/business-nest/public/live_events/pin/LOSGFT", 
   })
   .catch((err) => {
     console.log(err);
-  });
+  });*/
+fetch("https://api.quizacademy.io/business-nest/public/live_events/4337/results", {
+  headers: {
+    accept: "*/*",
+    "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    Referer: "https://go.quizacademy.io/",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+  },
+  body: null,
+  method: "OPTIONS",
+});
+fetch("https://api.quizacademy.io/business-nest/public/live_events/4337/results", {
+  headers: {
+    accept: "*/*",
+    "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "content-type": "application/json",
+    "sec-ch-ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "x-api-key": "jpmH06jXpX1d2dx4VIJmnaOmvEpXkJaV6rkJedek",
+    Referer: "https://go.quizacademy.io/",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+  },
+  body: '{"answers":[{"id":687524},{"id":687525}],"created_by":"6495c3e2-766d-4154-915d-ab01e9cdd56f","event_id":4337,"question_id":173625,"result":0,"time":2574}',
+  method: "POST",
+});
+
+/*
+fetch("https://api.quizacademy.io/business-nest/public/live_events/4337/results", {
+          "headers": {
+            "accept": "*\/*",
+            "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "content-type": "application/json",
+            "sec-ch-ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "x-api-key": "jpmH06jXpX1d2dx4VIJmnaOmvEpXkJaV6rkJedek",
+            Referer: "https://go.quizacademy.io/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+          },
+          "body": `{\"answers\":[${answers[q_id].right}],\"created_by\":\"${uuid}\",\"event_id\":${eventID},\"question_id\":${q_id},\"result\":1,\"time\":${time}}`,
+          "method": "POST"
+        });
+
+*/
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,24 +78,38 @@
       setTimeout(() => {
         let q_id = message.active_question_id;
         let time = Math.floor(Math.random() * (timeMax - timeMin + 1) + timeMin);
-        fetch("https://api.quizacademy.io/business-nest/public/live_events/4337/results", {
-          "headers": {
-            "accept": "*/*",
-            "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
-            "authorization": "eyJraWQiOiI5RUtaRzk0VnBOeUQ3cmhtZGhERGVpVWRiZ3pvaHlLVjNIZjlDem1Rd1NVPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlNWU1OWExMS1hOTFkLTQ2NTAtOWJkNy1lMzliOTZjMGI4MjQiLCJjb2duaXRvOmdyb3VwcyI6WyJDb3Vyc2VNYW5hZ2VyIl0sImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtY2VudHJhbC0xLmFtYXpvbmF3cy5jb21cL2V1LWNlbnRyYWwtMV9jQjlCeUlHbTMiLCJwaG9uZV9udW1iZXJfdmVyaWZpZWQiOmZhbHNlLCJjb2duaXRvOnVzZXJuYW1lIjoiZTVlNTlhMTEtYTkxZC00NjUwLTliZDctZTM5Yjk2YzBiODI0IiwiZ2l2ZW5fbmFtZSI6IlRlc3QiLCJhdWQiOiIxc2plZjg0ODQxYXVyMDE5Y3Q5MXVrcDRjcCIsImV2ZW50X2lkIjoiMTQ3MzNlYzMtYjcwOS00MDkzLTllZDEtZjJlZmY2ZWY0NTQwIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2OTk0MzYxMjMsIm5hbWUiOiJGUkVFIiwibmlja25hbWUiOiJTb25zdGlnZSIsImV4cCI6MTY5OTUxNzM2MywiaWF0IjoxNjk5NTEzNzYzLCJmYW1pbHlfbmFtZSI6IlVzZXIgKEJlbnV0emVyIGJpdHRlIG5pY2h0IMOkbmRlcm4hKSIsImVtYWlsIjoidGVzdC51c2VyQHF1aXphY2FkZW15LmRlIn0.lXJ5MS4lBOtyrPayAlo6KI3jI7h_FtMX0QKl3XE-F9zenQRzg2RNBIAtrIXMY4o-HRPxRS489t1aiBTJr-P-g_dxgUNPpF1IuLON9OxLUUbvOqQis_5wrK-EXkqQhSgpCGHRFJCpTMQo7jr9tJmaMAqWTp_LPvWtQqkfrQN0Y1CxTk6awh9vSO76IaQekq0OodepFN0CwnMGhjpM_kOmpzK9NGxIRtAkV-a2MeVanuWx1scL13Q3n-V86UBF3R_WqAH8LxB7oCyah7Lsl_pAFBHsrYdR6c5-JaJAvb6f-60VQ_2k6FZ6Z9UcTN7jkgLsP1m4DnW3bgv1QGT6ajHvoQ",
+
+        fetch(`https://api.quizacademy.io/business-nest/public/live_events/${eventID}/results`, {
+          headers: {
+            accept: "*/*",
+            "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            Referer: "https://go.quizacademy.io/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+          },
+          body: null,
+          method: "OPTIONS",
+        });
+
+        fetch(`https://api.quizacademy.io/business-nest/public/live_events/${eventID}/results`, {
+          headers: {
+            accept: "*/*",
+            "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
             "content-type": "application/json",
-            "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+            "sec-ch-ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
             "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-ch-ua-platform": '"Windows"',
             "sec-fetch-dest": "empty",
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-site",
             "x-api-key": "jpmH06jXpX1d2dx4VIJmnaOmvEpXkJaV6rkJedek",
-            "Referer": "https://go.quizacademy.io/",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
+            Referer: "https://go.quizacademy.io/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
           },
-          "body": `{\"answers\":[${answers[q_id].right}],\"created_by\":\"${uuid}\",\"event_id\":${eventID},\"question_id\":${q_id},\"result\":1,\"time\":${time}}`,
-          "method": "POST"
+          body: `{"answers":[${answers[q_id].right}],"created_by":"${uuid}","event_id":${eventID},"question_id":${q_id},"result":1,"time":${time}}`,
+          method: "POST",
         });
       }, 5000);
     } else if(message.status == "QUESTION_END") {
@@ -106,11 +120,13 @@
   };
 </script>
 
-<input type="text" placeholder="Code" bind:value={code}>
-<input type="text" placeholder="Name" bind:value={name}>
-<input type="number" placeholder="min Time" bind:value={timeMin}>
-<input type="number" placeholder="max Time" bind:value={timeMax}>
-<button on:click={click}>Start</button>
+<div class="container">
+  <input type="text" placeholder="Code" bind:value={code}>
+  <input type="text" placeholder="Name" bind:value={name}>
+  <input type="number" placeholder="min Time" bind:value={timeMin}>
+  <input type="number" placeholder="max Time" bind:value={timeMax}>
+  <button on:click={click}>Start</button>
+</div>
 
 <style>
 </style>
